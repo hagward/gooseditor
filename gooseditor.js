1ac61360aad1d92f52ef6784bf156426a4664809
hagward <hagward@kth.se>
2013-07-24 11:42:17 +0200

    Resolving them issues
    - Fixed issue #2 and #3.
    - Added an option for hiding the line number bar.
    - Added comments.
    - Added more literals for auto-completion.

--- gooseditor.js
+++ gooseditor.js
@@ -1,11 +1,15 @@
 // Here follows some configurable variables.
-var numLines = 1;
-var numSpacesIndent = 4;
-var documentLength = 0;
-var autoCompleteLiterals = {'(': ')', '[': ']', '{': '}'};
-var ignoreLiterals = {')': '(', ']': '[', '}': '{', ':': ':', ';': ';'};
-var indentLiterals = {'{': '}'};
 
+var numSpacesIndent = 4;
+var autoCompleteLiterals = {'(': ')', '[': ']', '{': '}', '\'': '\'', '"': '"'};
+var ignoreLiterals = {')': '(', ']': '[', '}': '{', ':': ':', ';': ';', '\'': '\'', '"': '"'};
+var indentLiterals = {'{': '}'};
+var showLineNumbers = true;
+
+// End of configuration.
+
+var numLines = 1; // keeps track of the number of lines
+var documentLength = 0; // used for calculating the diff in document length
 var editorDefaultHeight = parseInt($('#editor').css('height').slice(0, -2));
 var editorHeightIncrease = parseInt($('#editor').css('font-size').slice(0, -2)) + 3;
 var indentText = [];
@@ -14,6 +18,10 @@ for (var i = 0; i < numSpacesIndent; i++)
 indentText = indentText.join('');
 
 $(document).ready(function() {
+	$('#useLineNumbers').prop('checked', showLineNumbers);
+
+	// The keydown is only used to catch tab presses for now, as keypress
+	// doesn't register that particular key.
 	$('#editor').keydown(function(event) {
 		var keyCode = event.keyCode || event.which;
 		var cursorPos = $('#editor').textrange('get')['position'];
@@ -25,6 +33,7 @@ $(document).ready(function() {
 		}
 	});
 
+	// Handle all regular keypresses.
 	$('#editor').keypress(function(event) {
 		var keyCode = event.keyCode || event.which;
 		var cursorPos = $('#editor').textrange('get')['position'];
@@ -48,9 +57,13 @@ $(document).ready(function() {
 		updateLineNumberBar();
 	});
 
+	// Update the line number bar when the content of the editor change. For
+	// line number responsiveness.
 	$('#editor').bind('input propertychange', function() {
 		updateLineNumberBar();
 	});
+
+	// Here follows settings form listeners.
 
 	$('#fontSize').change(function() {
 		var fontSize = $('#fontSize').val();
@@ -63,9 +76,37 @@ $(document).ready(function() {
 		$('#lineNumbers').css('font-family', fontFamily);
 		$('#editor').css('font-family', fontFamily);
 	});
+
+	$('#useLineNumbers').change(function() {
+		showLineNumbers = $('#useLineNumbers').prop('checked');
+		if (!showLineNumbers) {
+			$('#lineNumbers').hide();
+		} else {
+			updateLineNumberBar();
+			$('#lineNumbers').show();
+		}
+	});
 });
 
+/**
+ * Adjusts the height of the line number bar and the editor to match
+ * 'newNumLines' number of lines or the height of the surrounding div,
+ * whichever is the greatest.
+ */
+function adjustHeight(newNumLines) {
+	var newHeight = Math.max(editorHeightIncrease * newNumLines, editorDefaultHeight);
+	$('#lineNumbers').css('height', (newHeight + 4) + 'px');
+	$('#editor').css('height', newHeight + 'px');
+}
+
+/**
+ * Updates the text on the line number bar to show the current number of lines
+ * in the document.
+ */
 function updateLineNumberBar() {
+	if (!showLineNumbers)
+		return;
+
 	var newNumLines = getNumLines();
 	var linesToInsert = [];
 	if (newNumLines > numLines) {
@@ -90,6 +131,19 @@ function updateLineNumberBar() {
 	numLines = newNumLines;
 }
 
+/**
+ * Returns the number of lines in the editor.
+ */
+function getNumLines() {
+	var text = $('#editor').val();
+	var lines = text.split(/\r|\r\n|\n/);
+	return lines.length;
+}
+
+/**
+ * Indents the code at 'position' to the correct indentation (based on the
+ * number of brackets) and adjusts the cursor position.
+ */
 function autoIndent(position) {
 	var indentArray = [];
 	var indentLevel = getIndentLevel(position);
@@ -110,29 +164,10 @@ function autoIndent(position) {
 	return indentLevel;
 }
 
-function autoComplete(position, insertedChar) {
-	var replacement = autoCompleteLiterals[insertedChar];
-	if (replacement) {
-		editorInsert(replacement, position, false);
-		$('#editor').textrange('setcursor', position);
-		return true;
-	}
-	return false;
-}
-
-function getLengthDiff() {
-	return $('#editor').val().length - documentLength;
-}
-
 /**
- * Returns the number of lines in the textarea.
+ * Returns the indentation level at 'position', based on the number of opened
+ * and closed brackets up to that point.
  */
-function getNumLines() {
-	var text = $('#editor').val();
-	var lines = text.split(/\r|\r\n|\n/);
-	return lines.length;
-}
-
 function getIndentLevel(position) {
 	var ups = 0;
 	var downs = 0;
@@ -148,14 +183,22 @@ function getIndentLevel(position) {
 }
 
 /**
- * Adjusts the height of the line number bar and the editor.
+ * Tries to auto-complete 'insertedChar' at 'position'.
  */
-function adjustHeight(newNumLines) {
-	var newHeight = Math.max(editorHeightIncrease * newNumLines, editorDefaultHeight);
-	$('#lineNumbers').css('height', newHeight + 'px');
-	$('#editor').css('height', newHeight + 'px');
+function autoComplete(position, insertedChar) {
+	var replacement = autoCompleteLiterals[insertedChar];
+	if (replacement) {
+		editorInsert(replacement, position, false);
+		$('#editor').textrange('setcursor', position);
+		return true;
+	}
+	return false;
 }
 
+/**
+ * Inserts 'text' at 'position' in the editor. If 'setCursor' is set, the
+ * cursor will be set to the end of the insertion.
+ */
 function editorInsert(text, position, setCursor) {
 	var doc = $('#editor').val();
 	var docPieces = [
